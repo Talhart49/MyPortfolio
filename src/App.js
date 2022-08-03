c08be34b182c764f2cd1bbfdf591dcc5896c3190
Talha <ranatalhaa01@gmail.com>
2022-08-04 00:04:35 +0500

    cursor added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { About, Footer, Header, Skills, Work } from './container';
 import { Navbar } from './components';
+import Cursor from './components/Cursor';
 import './App.scss';
 
 const App = () => (
   <div className='app'>
     <Navbar />
+    <Cursor />
+
     <Header />
     <About />
     <Work />
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -82,7 +82,7 @@ const Work = () => {
         {filterWork.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img src={urlFor(work.imgUrl)} alt={work.name} className='app__work-img01' />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
